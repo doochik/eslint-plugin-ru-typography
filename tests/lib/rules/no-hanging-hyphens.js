a13814c8605d66a49d1c488202be58651f6d525b
Aleksei Androsov <aandrosov@yandex-team.ru>
2022-07-25 12:10:14 +0300

    Check JSX. Fix #2

--- tests/lib/rules/no-hanging-hyphens.js
+++ tests/lib/rules/no-hanging-hyphens.js
@@ -3,7 +3,15 @@
 const rule = require("../../../lib/rules/no-hanging-hyphens"),
     RuleTester = require("eslint").RuleTester;
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+});
 
 ruleTester.run("no-hanging-hyphens", rule, {
     valid: [
@@ -30,6 +38,23 @@ ruleTester.run("no-hanging-hyphens", rule, {
         {
             code: "var foo = 'елки – палки'",
             output: "var foo = 'елки\u00A0– палки'",
+            errors: [{ message: "\" – \" should be preceded with \\u00A0(nbsp) symbol" }]
+        },
+        {
+            code: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (<span>елки – палки</span>);       
+                }
+            }
+            `,
+            output: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (<span>елки\u00A0– палки</span>);       
+                }
+            }
+            `,
             errors: [{ message: "\" – \" should be preceded with \\u00A0(nbsp) symbol" }]
         }
     ]
--- tests/lib/rules/no-hanging-prepositions.js
+++ tests/lib/rules/no-hanging-prepositions.js
@@ -3,7 +3,15 @@
 const rule = require("../../../lib/rules/no-hanging-prepositions"),
     RuleTester = require("eslint").RuleTester;
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+});
 
 ruleTester.run("no-hanging-prepositions", rule, {
     valid: [
@@ -12,6 +20,9 @@ ruleTester.run("no-hanging-prepositions", rule, {
         },
         {
             code: "var foo = 'елки и палки'"
+        },
+        {
+            code: "var foo = `елки и палки`"
         },
         {
             code: "var foo = 'елки и палки без леса'"
@@ -34,8 +45,55 @@ ruleTester.run("no-hanging-prepositions", rule, {
             errors: [{ message: "\" и \" should be followed with \\u00A0(nbsp) symbol" }]
         },
         {
+            code: "var foo = `елки и палки`",
+            output: "var foo = `елки и палки`",
+            errors: [{ message: "\" и \" should be followed with \\u00A0(nbsp) symbol" }]
+        },
+        {
             code: "var foo = 'елки и палки без леса'",
             output: "var foo = 'елки и палки без леса'",
+            errors: [{ message: "\" и \" should be followed with \\u00A0(nbsp) symbol" }]
+        },
+        {
+            code: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (<span>елки и палки</span>);       
+                }
+            }
+            `,
+            output: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (<span>елки и палки</span>);       
+                }
+            }
+            `,
+            errors: [{ message: "\" и \" should be followed with \\u00A0(nbsp) symbol" }]
+        },
+        {
+            code: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (
+                        <p>
+                            <span>елки и палки</span>    
+                        </p>
+                    );       
+                }
+            }
+            `,
+            output: `
+            class Component extends React.PureComponent {
+                render() {
+                    return (
+                        <p>
+                            <span>елки и палки</span>    
+                        </p>
+                    );       
+                }
+            }
+            `,
             errors: [{ message: "\" и \" should be followed with \\u00A0(nbsp) symbol" }]
         }
     ]
